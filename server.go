ab5ee05844b75f4129733e8fb75c4f53d554f7cd
shimpeiws <shimpeiws@gmail.com>
2017-03-04 13:24:31 +0900

    Remove middlesares

--- server.go
+++ server.go
@@ -4,16 +4,11 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	// Echo instance
 	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
 
 	// Route => handler
 	e.GET("/", func(c echo.Context) error {
