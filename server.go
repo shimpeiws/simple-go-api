5e3b1a1c27974b659f939d694e2710f56897e002
shimpeiws <shimpeiws@gmail.com>
2017-03-29 10:51:24 +0900

    Add api handling

--- server.go
+++ server.go
@@ -3,9 +3,101 @@ package main
 import (
 	"net/http"
 	"io/ioutil"
+	"bytes"
+	"encoding/json"
+	"fmt"
 
 	"github.com/labstack/echo"
 )
+
+type JwtToken struct {
+	Jwt string
+}
+
+type ItemList struct {
+	Data []Item
+}
+
+type Item struct {
+	Id string
+	Attributes ItemDetail
+}
+
+type ItemDetail struct {
+	Name string
+}
+
+func getJwtToken() (string, error) {
+	requestStr := `{"auth":{"email":"test@example.com","password":"test123"}}`
+	req, err := http.NewRequest(
+		"POST",
+		"http://localhost:3000/user_token",
+		bytes.NewBuffer([]byte(requestStr)),
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	jwtResponse := new(JwtToken)
+	fmt.Println(string(body))
+	jsonParseError := json.Unmarshal(body, &jwtResponse)
+	if jsonParseError != nil {
+		return "", jsonParseError
+	}
+
+	return jwtResponse.Jwt, nil
+}
+
+func getItems(token string) (*ItemList, error) {
+	req, err := http.NewRequest(
+		"GET",
+		"http://localhost:3000/items",
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", string("Bearer " + token))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	itemResponse := new(ItemList)
+	fmt.Println("getItemsResponse", string(body))
+	jsonParseError := json.Unmarshal(body, &itemResponse)
+	if jsonParseError != nil {
+		return nil, jsonParseError
+	}
+
+	return itemResponse, nil
+}
 
 func main() {
 	// Echo instance
@@ -31,6 +123,22 @@ func main() {
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
 		return c.String(http.StatusOK, string(body))
+	})
+
+	e.GET("/items", func(c echo.Context) error {
+		token, err := getJwtToken()
+		if err != nil {
+			return c.String(http.StatusForbidden, err.Error())
+		}
+		fmt.Println("token", token)
+
+		items, getItemErr := getItems(token)
+		if getItemErr != nil {
+			return c.String(http.StatusNotFound, getItemErr.Error())
+		}
+		fmt.Println("items", items)
+
+		return c.String(http.StatusOK, token)
 	})
 
 	// Start server
